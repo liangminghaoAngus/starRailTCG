7749c1ab6a6be983042c5f571a6a8cacf61276e6
liangminghaoangus <liangminghaoangus@163.com>
2023-06-04 14:50:38 +0800

    feat 增加展示界面的图片

--- go.mod
+++ go.mod
@@ -3,14 +3,21 @@ module starRailTCG
 go 1.19
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/ebitengine/purego v0.3.2 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b // indirect
+	github.com/gorilla/css v1.0.0 // indirect
 	github.com/hajimehoshi/ebiten/v2 v2.5.4 // indirect
 	github.com/jezek/xgb v1.1.0 // indirect
+	github.com/vanng822/css v1.0.1 // indirect
+	github.com/vanng822/go-premailer v1.20.2 // indirect
+	github.com/yohamta/furex/v2 v2.4.2 // indirect
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/exp/shiny v0.0.0-20230522175609-2e198f4a06a1 // indirect
 	golang.org/x/image v0.7.0 // indirect
 	golang.org/x/mobile v0.0.0-20230531173138-3c911d8e3eda // indirect
+	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sync v0.2.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 )
--- window.go
+++ window.go
@@ -3,16 +3,20 @@ package main
 import (
 	"fmt"
 	"image/color"
+	_ "image/gif"
+	"log"
 	"starRailTCG/config"
 	"strconv"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/yohamta/furex/v2"
 )
 
 type Game struct {
-	i   uint8
-	cfg *config.Config
+	i             uint8
+	cfg           *config.Config
+	OnboardScreen *furex.View
 }
 
 func NewGame(cfg *config.Config) *Game {
@@ -20,8 +24,10 @@ func NewGame(cfg *config.Config) *Game {
 	ebiten.SetWindowSize(cfg.ScreenWidth, cfg.ScreenHeight) //窗口大小
 	ebiten.SetWindowTitle(cfg.Title)                        //窗口标题
 
+	onboard := OnBoardView(cfg.ScreenWidth, cfg.ScreenHeight)
 	return &Game{
-		cfg: cfg,
+		cfg:           cfg,
+		OnboardScreen: onboard,
 	}
 }
 
@@ -36,6 +42,17 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	} else {
 		g.i = 0
 	}
+	// 加载背景图片
+	bgGIF, bgGIFFile, err := ebitenutil.NewImageFromFile("./static/imgs/onboard01.gif")
+	if err != nil {
+		log.Fatalf("ebitenutil.NewImageFromFile failed :%v", err)
+	}
+	log.Printf("bgGIFFile :%+v", bgGIFFile)
+	opt := &ebiten.DrawImageOptions{}
+	opt.GeoM.Scale(0.2, 0.2)
+	opt.GeoM.Translate(float64(g.cfg.ScreenWidth-bgGIFFile.Bounds().Dx())/2, float64(g.cfg.ScreenHeight-bgGIFFile.Bounds().Dy())/2)
+	screen.DrawImage(bgGIF, opt)
+
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f\nFPS: %0.2f", ebiten.ActualTPS(), ebiten.ActualFPS()))
 }
 
@@ -48,4 +65,34 @@ func Hex2RGB(color16 string, alpha uint8) color.RGBA {
 	g, _ := strconv.ParseInt(color16[2:4], 16, 18)
 	b, _ := strconv.ParseInt(color16[4:], 16, 10)
 	return color.RGBA{R: uint8(r), G: uint8(g), B: uint8(b), A: alpha}
+}
+
+func OnBoardView(width, height int) *furex.View {
+
+	onboardView := &furex.View{
+		Width:        width,
+		Height:       height,
+		Direction:    furex.Row,
+		Justify:      furex.JustifyCenter,
+		AlignItems:   furex.AlignItemCenter,
+		AlignContent: furex.AlignContentCenter,
+		Wrap:         furex.Wrap,
+	}
+
+	target := make([]*furex.View, 0)
+
+	// bgImgView := &furex.View{
+	// 	Width:  width,
+	// 	Height: height,
+	// }
+
+	// target = append(target, bgImgView)
+
+	// 制作按钮 todo
+
+	for _, item := range target {
+		onboardView.AddChild(item)
+	}
+	return onboardView
+
 }
