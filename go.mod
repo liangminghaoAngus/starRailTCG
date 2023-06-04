1b1b4a2724c60583b041c2a5d3dc9d40b7de5274
liangminghaoangus <liangminghaoangus@163.com>
2023-06-04 19:43:43 +0800

    feat 加入了按钮

--- go.mod
+++ go.mod
@@ -6,13 +6,14 @@ require (
 	github.com/PuerkitoBio/goquery v1.8.1 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/ebitengine/purego v0.3.2 // indirect
+	github.com/ebitenui/ebitenui v0.5.4 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
 	github.com/hajimehoshi/ebiten/v2 v2.5.4 // indirect
 	github.com/jezek/xgb v1.1.0 // indirect
 	github.com/vanng822/css v1.0.1 // indirect
 	github.com/vanng822/go-premailer v1.20.2 // indirect
-	github.com/yohamta/furex/v2 v2.4.2 // indirect
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/exp/shiny v0.0.0-20230522175609-2e198f4a06a1 // indirect
 	golang.org/x/image v0.7.0 // indirect
--- window.go
+++ window.go
@@ -8,15 +8,20 @@ import (
 	"starRailTCG/config"
 	"strconv"
 
+	"github.com/ebitenui/ebitenui"
+	"github.com/ebitenui/ebitenui/image"
+	"github.com/ebitenui/ebitenui/widget"
+	"github.com/golang/freetype/truetype"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"github.com/yohamta/furex/v2"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/gofont/goregular"
 )
 
 type Game struct {
 	i             uint8
 	cfg           *config.Config
-	OnboardScreen *furex.View
+	OnboardScreen *ebitenui.UI
 }
 
 func NewGame(cfg *config.Config) *Game {
@@ -24,7 +29,7 @@ func NewGame(cfg *config.Config) *Game {
 	ebiten.SetWindowSize(cfg.ScreenWidth, cfg.ScreenHeight) //窗口大小
 	ebiten.SetWindowTitle(cfg.Title)                        //窗口标题
 
-	onboard := OnBoardView(cfg.ScreenWidth, cfg.ScreenHeight)
+	onboard := NewOnBoardScreen()
 	return &Game{
 		cfg:           cfg,
 		OnboardScreen: onboard,
@@ -36,28 +41,38 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	g.i++
-	if g.i < 255 {
-		screen.Fill(Hex2RGB("#0dceda", g.i))
-	} else {
-		g.i = 0
-	}
+	// g.i++
+	// if g.i < 255 {
+	// 	screen.Fill(Hex2RGB("#0dceda", g.i))
+	// } else {
+	// 	g.i = 0
+	// }
+
+	screen.Fill(Hex2RGB("#0dceda", g.i))
+	sw, sh := screen.Bounds().Dx(), screen.Bounds().Dy()
 	// 加载背景图片
-	bgGIF, bgGIFFile, err := ebitenutil.NewImageFromFile("./static/imgs/onboard01.gif")
+	bgGIF, bgGIFFile, err := ebitenutil.NewImageFromFile("./static/imgs/onboard.gif")
 	if err != nil {
 		log.Fatalf("ebitenutil.NewImageFromFile failed :%v", err)
 	}
-	log.Printf("bgGIFFile :%+v", bgGIFFile)
 	opt := &ebiten.DrawImageOptions{}
-	opt.GeoM.Scale(0.2, 0.2)
-	opt.GeoM.Translate(float64(g.cfg.ScreenWidth-bgGIFFile.Bounds().Dx())/2, float64(g.cfg.ScreenHeight-bgGIFFile.Bounds().Dy())/2)
+	opt.GeoM.Scale(0.6, 0.6)
+	// opt.GeoM.Translate(float64(g.cfg.ScreenWidth-bgGIFFile.Bounds().Dx())/2, float64(g.cfg.ScreenHeight-bgGIFFile.Bounds().Dy())/2)
+	scale := ebiten.DeviceScaleFactor()
+	// log.Printf("scale :%+v", scale)
+	opt.GeoM.Scale(scale, scale)
+	opt.GeoM.Translate(float64(sw-bgGIFFile.Bounds().Dx()-40)/2, float64(sh-bgGIFFile.Bounds().Dy()-40)/2)
+	opt.Filter = ebiten.FilterLinear
+
 	screen.DrawImage(bgGIF, opt)
+	g.OnboardScreen.Draw(screen)
 
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f\nFPS: %0.2f", ebiten.ActualTPS(), ebiten.ActualFPS()))
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return g.cfg.ScreenWidth / 2, g.cfg.ScreenHeight / 2 //窗口分辨率
+	s := ebiten.DeviceScaleFactor()
+	return int(float64(outsideWidth) * s), int(float64(outsideHeight) * s)
 }
 
 func Hex2RGB(color16 string, alpha uint8) color.RGBA {
@@ -67,32 +82,81 @@ func Hex2RGB(color16 string, alpha uint8) color.RGBA {
 	return color.RGBA{R: uint8(r), G: uint8(g), B: uint8(b), A: alpha}
 }
 
-func OnBoardView(width, height int) *furex.View {
+func NewOnBoardScreen() *ebitenui.UI {
 
-	onboardView := &furex.View{
-		Width:        width,
-		Height:       height,
-		Direction:    furex.Row,
-		Justify:      furex.JustifyCenter,
-		AlignItems:   furex.AlignItemCenter,
-		AlignContent: furex.AlignContentCenter,
-		Wrap:         furex.Wrap,
+	buttonImage, _ := loadButtonImage()
+
+	face, _ := loadFont(24)
+
+	// construct a new container that serves as the root of the UI hierarchy
+	rootContainer := widget.NewContainer(
+		// the container will use a plain color as its background
+		// widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(color.NRGBA{0x13, 0x1a, 0x22, 0xff})),
+
+		// the container will use an anchor layout to layout its single child widget
+		widget.ContainerOpts.Layout(widget.NewAnchorLayout()),
+	)
+
+	// 开始游戏按钮 start game button
+	startGameBtn := widget.NewButton(
+		widget.ButtonOpts.WidgetOpts(
+			widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
+				HorizontalPosition: widget.AnchorLayoutPositionCenter,
+				VerticalPosition:   widget.AnchorLayoutPositionEnd,
+			}),
+			widget.WidgetOpts.MinSize(200, 80),
+		),
+		// specify the images to use
+		widget.ButtonOpts.Image(buttonImage),
+		widget.ButtonOpts.Text("START", face, &widget.ButtonTextColor{
+			Idle: color.RGBA{0xdf, 0xf4, 0xff, 0xff},
+		}),
+		// specify that the button's text needs some padding for correct display
+		widget.ButtonOpts.TextPadding(widget.Insets{
+			Left:   30,
+			Right:  30,
+			Top:    5,
+			Bottom: 5,
+		}),
+		// add a handler that reacts to clicking the button
+		widget.ButtonOpts.ClickedHandler(func(args *widget.ButtonClickedEventArgs) {
+			println("button clicked")
+		}),
+	)
+	// add the button as a child of the container
+	rootContainer.AddChild(startGameBtn)
+
+	// construct the UI
+	ui := &ebitenui.UI{
+		Container: rootContainer,
 	}
 
-	target := make([]*furex.View, 0)
+	return ui
+}
 
-	// bgImgView := &furex.View{
-	// 	Width:  width,
-	// 	Height: height,
-	// }
+func loadButtonImage() (*widget.ButtonImage, error) {
+	idle := image.NewNineSliceColor(color.NRGBA{R: 170, G: 170, B: 180, A: 255})
 
-	// target = append(target, bgImgView)
+	hover := image.NewNineSliceColor(color.NRGBA{R: 130, G: 130, B: 150, A: 255})
 
-	// 制作按钮 todo
+	pressed := image.NewNineSliceColor(color.NRGBA{R: 100, G: 100, B: 120, A: 255})
 
-	for _, item := range target {
-		onboardView.AddChild(item)
+	return &widget.ButtonImage{
+		Idle:    idle,
+		Hover:   hover,
+		Pressed: pressed,
+	}, nil
+}
+
+func loadFont(size float64) (font.Face, error) {
+	ttfFont, err := truetype.Parse(goregular.TTF)
+	if err != nil {
+		return nil, err
 	}
-	return onboardView
 
+	return truetype.NewFace(ttfFont, &truetype.Options{
+		Size:    size,
+		DPI:     72,
+		Hinting: font.HintingFull,
+	}), nil
 }
