09b5d423f09fceb907eb7e2c53c7e6cddbb47473
liangminghaoangus <liangminghaoangus@163.com>
2023-06-05 22:21:46 +0800

    feat 增加战斗随机骰

---
+++ battle.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"sort"
+	"starRailTCG/enums"
+	"time"
+)
+
+type Units []UnitItem
+
+func (u Units) Len() int {
+	return len(u)
+}
+
+func (u Units) Less(i, j int) bool {
+	f, s := u[i].Type, u[j].Type
+	return enums.GetAttributeInt(f) < enums.GetAttributeInt(s)
+}
+
+func (u Units) Swap(i, j int) {
+	u[i], u[j] = u[j], u[i]
+}
+
+type UnitItem struct {
+	Type     enums.AttributeInt
+	TypeName enums.Attribute
+}
+
+type Player struct {
+	Units           Units         // 拥有的战斗单元
+	OnBoardCharater interface{}   // todo 场上的角色
+	Charaters       []interface{} // todo 阵容中的角色
+	Active          bool          // 我的回合
+	EndRound        int           // 结束回合
+}
+
+// 开始回合
+func (p *Player) StartRound() {
+	p.randomUnits(enums.RoundUnit)
+}
+
+func (p *Player) randomUnits(rounds int) {
+	if len(p.Units) == 0 {
+		p.Units = make(Units, 0)
+	}
+
+	for i := 0; i < rounds; i++ {
+		rand.Seed(time.Now().UnixNano())
+		ind := rand.Intn(len(enums.AllAttributes))
+		enumType := enums.AllAttributes[ind]
+		enumTypeName := enums.GetAttributeStr(enumType)
+		p.Units = append(p.Units, UnitItem{Type: enumType, TypeName: enumTypeName})
+	}
+	sort.Sort(p.Units)
+	log.Printf("unit list :%+v", p.Units)
+}
--- enums/attribute.go
+++ enums/attribute.go
@@ -1,10 +1,25 @@
 package enums
 
+type AttributeInt int
+
+// 万能 物理 火 风 冰 雷 虚数 量子
+const (
+	AttributeAllInt AttributeInt = iota + 1
+	AttributePhysicsInt
+	AttributeFireInt
+	AttributeWindInt
+	AttributeIceInt
+	AttributeLightingInt
+	AttributeQuantumInt
+	AttributeImaginaryInt
+)
+
 // 命途属性
 type Attribute string
 
 // 物理 火 风 冰 雷 虚数 量子
 const (
+	AttributeAll       Attribute = "万能"
 	AttributePhysics   Attribute = "物理"
 	AttributeFire      Attribute = "火"
 	AttributeWind      Attribute = "风"
@@ -13,3 +28,47 @@ const (
 	AttributeQuantum   Attribute = "量子"
 	AttributeImaginary Attribute = "虚数"
 )
+
+var attributeIntStr = map[AttributeInt]Attribute{
+	AttributeAllInt:       AttributeAll,
+	AttributePhysicsInt:   AttributePhysics,
+	AttributeFireInt:      AttributeFire,
+	AttributeWindInt:      AttributeWind,
+	AttributeLightingInt:  AttributeLighting,
+	AttributeIceInt:       AttributeIce,
+	AttributeQuantumInt:   AttributeQuantum,
+	AttributeImaginaryInt: AttributeImaginary,
+}
+
+var attributeStrInt = map[Attribute]AttributeInt{
+	AttributeAll:       AttributeAllInt,
+	AttributeFire:      AttributeFireInt,
+	AttributeIce:       AttributeIceInt,
+	AttributeImaginary: AttributeImaginaryInt,
+	AttributeLighting:  AttributeLightingInt,
+	AttributePhysics:   AttributePhysicsInt,
+	AttributeQuantum:   AttributeQuantumInt,
+	AttributeWind:      AttributeWindInt,
+}
+
+var AllAttributes = []AttributeInt{AttributeAllInt, AttributePhysicsInt, AttributeFireInt, AttributeWindInt, AttributeLightingInt, AttributeIceInt, AttributeQuantumInt, AttributeImaginaryInt}
+
+var RoundUnit int = 8
+
+var MaxUnits int = 16
+
+func GetAttributeStr(attrInt AttributeInt) Attribute {
+	if str, ok := attributeIntStr[attrInt]; ok {
+		return str
+	} else {
+		return ""
+	}
+}
+
+func GetAttributeInt(attrStr Attribute) AttributeInt {
+	if num, ok := attributeStrInt[attrStr]; ok {
+		return num
+	} else {
+		return -1
+	}
+}
